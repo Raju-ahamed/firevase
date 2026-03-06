b2bcb9edb86f870894fb419879044f8f1c6cb89f
Raju-ahamed <rajuahamed7320@gmail.com>
2026-03-06 13:01:38 +0600

    new start

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,8 +1,9 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../firebase/firebase.init';
 const Login = () => {
     const [user, setUser] = useState(null);
+    
     const provider = new GoogleAuthProvider();
     const handleOnClick = () => {
         console.log("clicked");
@@ -14,6 +15,14 @@ const Login = () => {
         })
 
     }
+    const handleSignOut =()=>{
+        signOut(auth).then(()=>{
+            setUser(null);
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
     return (
         <div>
             <p>Please Log in with gmail</p>
@@ -21,7 +30,8 @@ const Login = () => {
             <div>
                 <h3>{user?.displayName}</h3>
                 <p>Email:{user?.email}</p>
-                <img src={user.photoURL} alt="image" />
+                <img src={user?.photoURL} alt="image" /><br />
+                <button onClick={handleSignOut}>sgin out</button>
             </div>
         </div>
     );
