b24082aacaffd3f8e6a390fd18c04a3b02dc1088
Raju-ahamed <rajuahamed7320@gmail.com>
2026-03-06 12:51:43 +0600

    new start

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,24 +1,28 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import { auth } from '../firebase/firebase.init';
 const Login = () => {
-const provider = new GoogleAuthProvider();
-
-    const handleOnClick=()=>{
+    const [user, setUser] = useState(null);
+    const provider = new GoogleAuthProvider();
+    const handleOnClick = () => {
         console.log("clicked");
-        signInWithPopup(auth,provider).then(resut=>{
-            console.log(resut)
-        }).catch(error=>{
+        signInWithPopup(auth, provider).then(resut => {
+            console.log(resut.user);
+            setUser(resut.user)
+        }).catch(error => {
             console.log(error)
         })
-       
+
     }
     return (
         <div>
             <p>Please Log in with gmail</p>
             <button onClick={handleOnClick}>plz Sign in with Golgle</button>
-            <button>facebook sgin in</button>
-            <button>twetter sgin in</button>
+            <div>
+                <h3>{user?.displayName}</h3>
+                <p>Email:{user?.email}</p>
+                <img src={user.photoURL} alt="image" />
+            </div>
         </div>
     );
 };
